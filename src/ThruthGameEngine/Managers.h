18611ae8c562600cc908f414d269cee8a0668162
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2024-05-02 20:06:33 +0900

    Physx 사용을 위한 준비 -> vcpkg를 통한 Physx 라이브러리 붙이기

--- src/ThruthGameEngine/Managers.h
+++ src/ThruthGameEngine/Managers.h
@@ -6,6 +6,7 @@ class TimeManager;
 class InputManager;
 class EventManager;
 class SceneManager;
+class PhysicsManager;
 
 class Managers
 {
@@ -13,7 +14,8 @@ public:
 	std::shared_ptr<TimeManager> m_timeManager;
 	std::shared_ptr<InputManager> m_inputManager;
 	std::shared_ptr<EventManager> m_eventManager;
-	std::shared_ptr<SceneManager> m_sceneManager; 
+	std::shared_ptr<SceneManager> m_sceneManager;
+	std::shared_ptr<PhysicsManager> m_physXManager;
 
 public:
 	// friend class Processor;
@@ -21,21 +23,21 @@ public:
 	~Managers();
 
 public:
-	void Initialize(HWND _hwnd);
-	void Update();
-	void LateUpdate();
-	void FixedUpdate();
-	void Render();
+	void Initialize(HWND _hwnd) ;
+	void Update() const;
+	void LateUpdate() const;
+	void FixedUpdate() const;
+	void Render() const;
 
-	void Finalize();
+	void Finalize() const;
 
-	inline std::shared_ptr<TimeManager> Time() { return m_timeManager; };
-	inline std::shared_ptr<InputManager> Input() { return m_inputManager; };
-	inline std::shared_ptr<EventManager> Event() { return m_eventManager; };
-	inline std::shared_ptr<SceneManager> Scene() { return m_sceneManager; };
+	inline std::shared_ptr<TimeManager> Time() const  { return m_timeManager; };
+	inline std::shared_ptr<InputManager> Input() const  { return m_inputManager; };
+	inline std::shared_ptr<EventManager> Event() const  { return m_eventManager; };
+	inline std::shared_ptr<SceneManager> Scene() const  { return m_sceneManager; };
 
 private:
 	void CreateManagers();
-	void InitlizeManagers(HWND _hwnd);
+	void InitlizeManagers(HWND _hwnd) const;
 };
 
---
+++ src/ThruthGameEngine/PhysicsManager.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Headers.h"
+
+// 라이브러리에 경고가 너무 많다
+// 나중에 이 부분 수정 예정
+// 뭔가 다른 방법이 있겠지
+#pragma warning(push)
+#pragma warning(disable: 26495)
+#pragma warning(disable: 33010)
+#pragma warning(disable: 6297)
+#include "physx/PxPhysicsAPI.h"
+#pragma warning(pop)
+
+/// <summary>
+/// PhysX 라이브러리를 사용하는 매니저
+/// 모든 PhysX 기능은 이 라이브러리를 통해 실행 될 것이다.
+/// </summary>
+class PhysicsManager
+{
+private:
+	// Foundation 생성
+	physx::PxDefaultAllocator m_allocator;
+	physx::PxDefaultErrorCallback m_errorCallback;
+	physx::PxFoundation* m_foundation;
+
+	physx::PxPhysics* m_physics;
+
+	physx::PxDefaultCpuDispatcher* m_dispatcher;
+	physx::PxScene* m_scene;
+	physx::PxMaterial* m_material;
+
+	// 디버거
+	physx::PxPvd* m_pvd;
+
+public:
+	PhysicsManager();
+	~PhysicsManager();
+
+	void Initalize();
+	void Finalize();
+
+private:
+	void SetPhysics();
+	void SetScene();
+	void SetMaterial();
+};
+
