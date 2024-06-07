97231a5a8a2ba32b85bdf025c92dafdca8611565
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2024-06-07 15:13:31 +0900

    완전안 데모 씬 로드 성공

--- src/EngineDemo/Bullet.cpp
+++ src/EngineDemo/Bullet.cpp
@@ -1,28 +1,28 @@
-// #include "Bullet.h"
-// #include "RigidBody.h"
-// #include "BoxCollider.h"
-// #include "BulletMove.h"
-// #include "CapsuleCollider.h"
-// #include "SphereCollider.h"
-// #include "Mesh.h"
-// 
-// Bullet::Bullet()
-// {
-// 	m_layer = 2;
-// }
-// 
-// Bullet::~Bullet()
-// {
-// 
-// }
-// 
-// void Bullet::Initailize()
-// {
-// 	__super::Initailize();
-// 	AddComponent<BulletMove>();
-// 	AddComponent<Truth::RigidBody>();
-// 	AddComponent<Truth::SphereCollider>(2.0f, false);
-// 	AddComponent<Truth::Mesh>(L"debugCube/debugCube");
-// 	SetPosition(Vector3{ 0.0f, 50.0f, 50.0f });
-// 	SetScale(Vector3(4.0f, 4.0f, 4.0f));
-// }
+#include "Bullet.h"
+#include "RigidBody.h"
+#include "BoxCollider.h"
+#include "BulletMove.h"
+#include "CapsuleCollider.h"
+#include "SphereCollider.h"
+#include "Mesh.h"
+
+Bullet::Bullet()
+{
+	m_layer = 2;
+}
+
+Bullet::~Bullet()
+{
+
+}
+
+void Bullet::Initailize()
+{
+	__super::Initailize();
+	AddComponent<BulletMove>();
+	AddComponent<Truth::RigidBody>();
+	AddComponent<Truth::SphereCollider>(2.0f, false);
+	AddComponent<Truth::Mesh>(L"debugCube/debugCube");
+	SetPosition(Vector3{ 0.0f, 50.0f, 50.0f });
+	SetScale(Vector3(4.0f, 4.0f, 4.0f));
+}
--- src/EngineDemo/Bullet.h
+++ src/EngineDemo/Bullet.h
@@ -1,15 +1,15 @@
-// #pragma once
-// #include "Headers.h"
-// #include "Entity.h"
-// 
-// class Bullet 
-//     : public Truth::Entity
-// {
-// 	GENERATE_CLASS_TYPE_INFO(Bullet)
-// 
-// public:
-// 	Bullet();
-// 	virtual ~Bullet();
-// 	virtual void Initailize() override;
-// };
-// 
+#pragma once
+#include "Headers.h"
+#include "Entity.h"
+
+class Bullet 
+    : public Truth::Entity
+{
+	GENERATE_CLASS_TYPE_INFO(Bullet)
+
+public:
+	Bullet();
+	virtual ~Bullet();
+	virtual void Initailize() override;
+};
+
