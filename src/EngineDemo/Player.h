f2909559d443d0e276cb05dc45eac86ff5133e18
Pableet <164502578+Pableet@users.noreply.github.com>
2024-06-18 15:19:06 +0900

    오류 안나게 고침

--- src/EngineDemo/Player.h
+++ src/EngineDemo/Player.h
@@ -13,8 +13,8 @@ private:
 	void serialize(Archive& _ar, const unsigned int _file_version);
 
 private:
-	PROPERTY(maxSpeed);
-	float4 m_maxSpeed;
+	PROPERTY(speed);
+	float4 m_speed;
 
 	PROPERTY(jumpForce);
 	float4 m_jumpForce;
