c239b6cf295b9e81dda8493b83f0339803d09388
Pableet <164502578+Pableet@users.noreply.github.com>
2024-07-04 13:52:24 +0900

    Revert "플레이어카메라 스크롤 추가해야됨"
    This reverts commit d449f119903fc3245cf918c5c4f64d0bb336ada9.

--- src/EngineDemo/PlayerCamera.cpp
+++ src/EngineDemo/PlayerCamera.cpp
@@ -46,15 +46,6 @@ void PlayerCamera::Start()
 
 void PlayerCamera::LateUpdate()
 {
-	if (GetKey(KEY::UPBUTTON))
-	{
-		m_cameraDistance += 0.1f;
-	}
-	if (GetKey(KEY::DOWNBUTTON))
-	{
-		m_cameraDistance -= 0.1f;
-	}
-
 	if (!m_isLockOn == GetKeyDown(KEY::Q))
 	{
 		m_isLockOn = true;
