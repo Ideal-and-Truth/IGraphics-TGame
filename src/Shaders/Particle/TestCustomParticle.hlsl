86c375c8d9e5c0fb1fb3236454609c35ec74bd01
chrysos8201 <chrysos8201@naver.com>
2024-09-16 19:21:56 +0900

    이펙트 나오긴 하는데 색상 적용 X

--- src/Shaders/Particle/TestCustomParticle.hlsl
+++ src/Shaders/Particle/TestCustomParticle.hlsl
@@ -19,75 +19,27 @@ float4 PSMain(VSOutput input) : SV_TARGET
         Ideal_SimpleNoise_float(uvTiled, 50.0, noise);
     }
     // Step 함수 적용
-    float4 stepData;
+    float stepData;
     {
-        //float4 edge = float4(noise, noise, noise, noise);
-        //float4 InData = float4(0.5, 0.5, 0.5, 0.5);
-        //Step_float4(edge, InData, stepData);
+      
         float tempTime = 1 - g_Time;
         float s = step(noise, tempTime);
-        stepData = float4(s,s,s,s);
+        stepData = s;
     }
-    
-    // Step 함수 적용 (노이즈 기반 마스크)
-    //float4 edge = float4(0.5, 0.5, 0.5, 1.0);
-    //float4 noiseMask;
-    //Step_float4(edge, float4(noise, noise, noise, 1.0), noiseMask);
-    //
-    //// 텍스처 샘플링
-    //float4 texColor = ParticleTexture.Sample(LinearWrapSampler, scrolledUV);
-
-    // 최종 색상 출력 (노이즈와 텍스처 결합)
-    //return lerp(texColor, float4(1.0, 1.0, 1.0, 1.0), noiseMask);
-    //return noiseMask;
-    //return texColor;
 
     float dis_amount = 0.2f;
     float gradientNoise;
     Ideal_GradientNoise_float(scrolledUV, 10, gradientNoise);
     
-    float2 outValue = lerp(input.UV, float2(gradientNoise, gradientNoise), dis_amount);
-    float4 SampleTexture = ParticleTexture.Sample(LinearClampSampler, outValue);
-    //float4 SampleTexture = ParticleTexture.Sample(LinearClampSampler, input.UV);
-
-
-
+    //float2 outValue = lerp(input.UV, float2(gradientNoise, gradientNoise), dis_amount);
+    float2 outValue = lerp(scrolledUV, gradientNoise, dis_amount);
+    //float4 SampleTexture = ParticleTexture.Sample(LinearClampSampler, outValue);
+    float4 SampleTexture = ParticleTexture.Sample(LinearClampSampler, input.UV);
     float4 data = mul(stepData, SampleTexture);
-    data = mul(float4(1, 1, 1, 1), data);
-    
-    // custom color
-    data = mul(data, float4(0.4, 0, 0, 1));
+    data = mul(float4(1, 1, 1, 0), data);
+    data = mul(float4(1, 1, 1, 0), data);
+    data = mul(float4(0.9245, 0.3968, 0.7052, 0), data);
     return data;
-    
-
-
-
-
-
-
-
-
-
-
-
-
-    //float time = 1.f;
-    //float4 customColor = float4(1,0,0,1);
-    //float2 scroll = float2(0.2, 0);
-    //float dis_amount = 1;
-    //
-    //
-    //float2 Out;
-    //TilingAndOffset_float(input.UV, float2(1,1), scroll, Out);
-    //SimpleNoise_float(Out, 50, Out);
-    //step(Out, 0);
-    //
-    //
-    //float4 color = ParticleTexture.Sample(LinearWrapSampler, input.UV);
-    //if(color.x < 0.001 && color.y < 0.001 && color.z < 0.001)
-    //    color.w = 0.f;
-    //return color;
-    //return float4(1, 0, 0, 0.5); // SV_POSITION 값을 반환
 }
 
 #endif
