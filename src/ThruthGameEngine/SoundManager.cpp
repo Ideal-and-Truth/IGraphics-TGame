f416e9953ec80aee9165cb86a712cc187a76524a
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2024-10-04 16:29:54 +0900

    사운드 매니져 구현 완료

--- src/ThruthGameEngine/SoundManager.cpp
+++ src/ThruthGameEngine/SoundManager.cpp
@@ -4,8 +4,6 @@
 
 Truth::SoundManager::SoundManager()
 	: m_system(nullptr)
-	, m_sound(nullptr)
-	, m_channel(nullptr)
 {
 	FMOD_RESULT result;
 	result = FMOD::System_Create(&m_system);
@@ -32,9 +30,55 @@ void Truth::SoundManager::Update()
 
 void Truth::SoundManager::Finalize()
 {
-
+	for (auto& s : m_soundMap)
+	{
+		s.second->release();
+	}
+	m_system->release();
 }
 
-void Truth::SoundManager::CreateSound(fs::path _path, const std::string& _alias)
+void Truth::SoundManager::CreateSound(fs::path _path, bool _isLoop)
 {
+	std::unordered_map<fs::path, FMOD::Sound*>::iterator itr = m_soundMap.find(_path);
+	if (itr == m_soundMap.end())
+	{
+		return;
+	}
+
+	FMOD_RESULT result;
+	if (_isLoop == true)
+	{
+		result = m_system->createSound(_path.generic_string().c_str(), FMOD_LOOP_NORMAL, 0, &m_soundMap[_path]);
+	}
+	else
+	{
+		result = m_system->createSound(_path.generic_string().c_str(), FMOD_LOOP_OFF, 0, &m_soundMap[_path]);
+	}
+}
+
+void Truth::SoundManager::SetVolum(int _channel, float _vol)
+{
+	m_channel[_channel]->setVolume(_vol);
+}
+
+void Truth::SoundManager::Play(fs::path _path, bool _canReduplication, int _channel)
+{
+	std::unordered_map<fs::path, FMOD::Sound*>::iterator itr = m_soundMap.find(_path);
+	if (itr == m_soundMap.end())
+	{
+		return;
+	}
+	FMOD_RESULT result;
+
+	bool isPlaying = false;
+	m_channel[_channel]->isPlaying(&isPlaying);
+
+	if (_canReduplication)
+	{
+		result = m_system->playSound((*itr).second, nullptr, false, &m_channel[_channel]);
+	}
+	else if (!_canReduplication && !isPlaying)
+	{
+		result = m_system->playSound((*itr).second, nullptr, false, &m_channel[_channel]);
+	}
 }
