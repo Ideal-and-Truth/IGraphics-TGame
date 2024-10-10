5908a81e88238f68f2534300c1d8f9a4a1199f5e
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2024-10-10 11:29:09 +0900

    button 클래스를 새로 만들지 않고 UI 클래스에 버튼 기능을 추가함

--- src/ThruthGameEngine/UI.cpp
+++ src/ThruthGameEngine/UI.cpp
@@ -8,59 +8,78 @@ BOOST_CLASS_EXPORT_IMPLEMENT(Truth::UI)
 
 Truth::UI::UI()
 	: m_sprite()
-	, m_texturePath(L"../Resources/Textures/PlayerRe/T_eyes_BaseMap.png")
-	, m_scale{1.0f, 1.0f}
+	, m_texturePath()
+	, m_scale{ 1.0f, 1.0f }
 	, m_isActive(true)
 	, m_alpha(1.0f)
 	, m_zDepth(0.0f)
 {
+
 }
 
 Truth::UI::~UI()
 {
-	m_managers.lock()->Graphics()->DeleteSprite(m_sprite);
+	for (uint32 i = 0; i < 3; i++)
+	{
+		if (m_sprite[i] != nullptr)
+		{
+			m_managers.lock()->Graphics()->DeleteSprite(m_sprite[i]);
+		}
+	}
 }
 
 void Truth::UI::Initalize()
 {
 	auto gp = m_managers.lock()->Graphics();
-
-	m_sprite = gp->CreateSprite();
-
-	auto tex = gp->CreateTexture(m_texturePath);
-	m_sprite->SetTexture(tex->m_texture);
-
-	m_sprite->SetScale(m_scale);
-	m_sprite->SetPosition(m_position);
-	m_sprite->SetActive(m_isActive);
-	m_sprite->SetAlpha(m_alpha);
-	m_sprite->SetZ(m_zDepth);
-	m_sprite->SetSampleRect({ 0, 0, tex->w, tex->h });
-}
-
-void Truth::UI::Awake()
-{
-
+	for (uint32 i = 0; i < 3; i++)
+	{
+		if (!m_texturePath[i].empty())
+		{
+			m_sprite[i] = gp->CreateSprite();
+			auto tex = gp->CreateTexture(m_texturePath[i]);
+			m_texture[i] = tex;
+			m_sprite[i]->SetTexture(tex->m_texture);
+			m_sprite[i]->SetScale(m_scale);
+			m_sprite[i]->SetPosition(m_position);
+			m_sprite[i]->SetActive(m_isActive);
+			m_sprite[i]->SetAlpha(m_alpha);
+			m_sprite[i]->SetZ(m_zDepth);
+			m_sprite[i]->SetSampleRect({ 0, 0, tex->w, tex->h });
+		}
+	}
 }
 
 void Truth::UI::Update()
 {
 
 }
+
+void Truth::UI::OnMouseOver()
+{
+	m_state = BUTTON_STATE::OVER;
+}
+
+void Truth::UI::OnMouseClick()
+{
+	m_state = BUTTON_STATE::DOWN;
+}
+
+void Truth::UI::OnMouseUp()
+{
+	m_state = BUTTON_STATE::IDEL;
+}
+
 #ifdef EDITOR_MODE
- 
 void Truth::UI::EditorSetValue()
 {
 	auto gp = m_managers.lock()->Graphics();
-
-	auto tex = gp->CreateTexture(m_texturePath);
-	m_sprite->SetTexture(tex->m_texture);
-
-	m_sprite->SetScale(m_scale);
-	m_sprite->SetPosition(m_position);
-	m_sprite->SetActive(m_isActive);
-	m_sprite->SetAlpha(m_alpha);
-	m_sprite->SetZ(m_zDepth);
-	m_sprite->SetSampleRect({ 0, 0, tex->w, tex->h });
+	for (uint32 i = 0; i < 3; i++)
+	{
+		m_sprite[i]->SetScale(m_scale);
+		m_sprite[i]->SetPosition(m_position);
+		m_sprite[i]->SetActive(m_isActive);
+		m_sprite[i]->SetAlpha(m_alpha);
+		m_sprite[i]->SetZ(m_zDepth);
+	}
 }
 #endif // IFDE
--- src/ThruthGameEngine/UI.h
+++ src/ThruthGameEngine/UI.h
@@ -21,19 +21,36 @@ namespace Truth
 		template<class Archive>
 		void load(Archive& ar, const unsigned int file_version);
 
-	private:
-		std::shared_ptr<Ideal::ISprite> m_sprite;
+		enum class BUTTON_STATE
+		{
+			IDEL,
+			OVER,
+			DOWN,
+			END,
+		};
 
-		PROPERTY(texturePath);
-		std::wstring m_texturePath;
+	private:
+		std::shared_ptr<Ideal::ISprite> m_sprite[3];
+
+		fs::path m_texturePath[3];
+
+		BUTTON_STATE m_state;
+
+		PROPERTY(texture);
+		std::shared_ptr<Texture> m_texture[3];
+
 		PROPERTY(scale);
 		Vector2 m_scale;
+
 		PROPERTY(position);
 		Vector2 m_position;
+
 		PROPERTY(isActive);
 		bool m_isActive;
+
 		PROPERTYM(alpha, 0.0f, 1.0f);
 		float m_alpha;
+
 		PROPERTYM(zDepth, 0.0f, 1.0f);
 		float m_zDepth;
 
@@ -45,11 +62,12 @@ namespace Truth
 		METHOD(Initalize);
 		virtual void Initalize() override;
 
-		METHOD(Awake);
-		virtual void Awake() override;
-
 		METHOD(Update);
 		virtual void Update() override;
+
+		void OnMouseOver();
+		void OnMouseClick();
+		void OnMouseUp();
 
 #ifdef EDITOR_MODE
 		virtual void EditorSetValue();
@@ -61,7 +79,11 @@ namespace Truth
 	void Truth::UI::save(Archive& _ar, const unsigned int file_version) const
 	{
 		_ar& boost::serialization::base_object<Component>(*this);
-		_ar& m_texturePath;
+
+		std::string texPath;
+		_ar& texPath;
+		m_texturePath = fs::path(texPath);
+
 		_ar& m_scale;
 		_ar& m_position;
 		_ar& m_isActive;
@@ -73,7 +95,8 @@ namespace Truth
 	void Truth::UI::load(Archive& _ar, const unsigned int file_version)
 	{
 		_ar& boost::serialization::base_object<Component>(*this);
-		_ar& m_texturePath;
+
+		_ar& m_texturePath.generic_string();
 		_ar& m_scale;
 		_ar& m_position;
 		_ar& m_isActive;
