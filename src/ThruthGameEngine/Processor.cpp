64ea3e6a5c270097026d6b00bbe7674591e92f01
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2024-10-31 19:22:25 +0900

    타이틀 씬 추가 및 스테이지 1 몬스터 배치

--- src/ThruthGameEngine/Processor.cpp
+++ src/ThruthGameEngine/Processor.cpp
@@ -108,8 +108,6 @@ void Processor::Initialize(HINSTANCE _hInstance)
 #ifdef EDITOR_MODE
 
 	Truth::UnityParser up(m_manager->Graphics().get());
-	// up.SetRootDir("E:\\Projects\\SampleUnity\\parsingTest");
-	// up.ParseUnityFile("E:\\Projects\\SampleUnity\\parsingTest\\Assets\\Scenes\\SampleScene.unity");
 	// up.SetRootDir("E:\\Projects\\ChronosUnity\\Kronos_IAT_Unity\\Cronos_URP");
 	// up.ParseUnityFile("E:\\Projects\\ChronosUnity\\Kronos_IAT_Unity\\Cronos_URP\\Assets\\Scenes\\ArtRoom\\FinalScene\\0_TitleTest.unity");
 	// up.ParseUnityFile("E:\\Projects\\ChronosUnity\\Kronos_IAT_Unity\\Cronos_URP\\Assets\\Scenes\\ArtRoom\\FinalScene\\1_HN_Scene2.unity");
