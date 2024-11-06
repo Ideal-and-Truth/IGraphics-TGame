a95e4497eef4cfc64f1d41de5cae98f7353d4e02
Hyenghwan Kim <71327618+hedwig3798@users.noreply.github.com>
2024-11-06 21:13:31 +0900

    변경된 코드 및 xml데이터

--- src/EngineDemo/EngineDemo.cpp
+++ src/EngineDemo/EngineDemo.cpp
@@ -70,7 +70,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //  	processor->SaveScene(testScene);
 
 	///...///
- 	processor->LoadScene(L"../Scene/Stage2.scene");
+ 	processor->LoadScene(L"../Scene/StageBoss.scene");
 	///...///
 
 	processor->Loop();
